caffecce5ebf348949a8b0854c52533bd51bdff1
marcos012 <marcospaulo.ad@gmail.com>
2021-04-27 20:04:15 -0300

    Cria cadastro de horario para regar uma planta

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect, useState } from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
-import colors from '../styles/colors';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
-
 import userImage from '../assets/marcos.png';
+import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 export default function Header() {
+    const [userName, setUserName] = useState<string>();
+
+    useEffect(() => {
+      async function loadStorageUserName() {
+        const user = await AsyncStorage.getItem('@plantmanager:user');
+  
+        setUserName(user || '');
+      }
+  
+      loadStorageUserName()
+    }, [userName])
+
     return (
         <View style={styles.container}>
             <View>
                 <Text style={styles.greeting}>Olá</Text>
-                <Text style={styles.userName}>Marcos</Text>
+                <Text style={styles.userName}>{userName}</Text>
             </View>
 
             <Image style={styles.image} source={userImage} />
--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,27 +1,40 @@
-import { createStackNavigator } from '@react-navigation/stack';
-import React from 'react';
-import Confirmation from '../pages/Confirmation';
-import PlantSelect from '../pages/PlantSelect';
-import { UserIdentification } from '../pages/UserIdentification';
-import { Welcome } from '../pages/Welcome';
-import colors from '../styles/colors';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
 
+import { Welcome } from "../pages/Welcome";
+import { UserIdentification } from "../pages/UserIdentification";
+import { PlantSave } from "../pages/PlantSave";
+import { MyPlants } from "../pages/MyPlants";
+import AuthRoutes from "./tabs.routes";
+
+import colors from "../styles/colors";
+import Confirmation from "../pages/Confirmation";
 const stackRoutes = createStackNavigator();
 
 const AppRoutes: React.FC = () => (
-    <stackRoutes.Navigator
-        headerMode="none"
-        screenOptions={{
-            cardStyle: {
-                backgroundColor: colors.white,
-            },
-        }}
-    >
-        <stackRoutes.Screen name="Welcome" component={Welcome} />
-        <stackRoutes.Screen name="UserIdentification" component={UserIdentification} />
-        <stackRoutes.Screen name="Confirmation" component={Confirmation} />
-        <stackRoutes.Screen name="PlantSelect" component={PlantSelect} />
-    </stackRoutes.Navigator>
-)
+  <stackRoutes.Navigator
+    headerMode="none"
+    screenOptions={{
+      cardStyle: {
+        backgroundColor: colors.white,
+      },
+    }}
+  >
+    <stackRoutes.Screen name="Welcome" component={Welcome} />
+
+    <stackRoutes.Screen
+      name="UserIdentification"
+      component={UserIdentification}
+    />
+
+    <stackRoutes.Screen name="Confirmation" component={Confirmation} />
+
+    <stackRoutes.Screen name="PlantSelect" component={AuthRoutes} />
+
+    <stackRoutes.Screen name="PlantSave" component={PlantSave} />
+
+    <stackRoutes.Screen name="MyPlants" component={AuthRoutes} />
+  </stackRoutes.Navigator>
+);
 
 export default AppRoutes;
